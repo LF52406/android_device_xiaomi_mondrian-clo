11aee8f1b4962613b77515180d874b7218f65aed
LF5 <lf524prod24061993@gmail.com>
2026-06-21 02:22:22 +0300

    Change PRODUCT_NAME and add boot animation settings
    Updated product name and added boot animation resolution.

--- mondrian.mk
+++ mondrian.mk
@@ -2,13 +2,19 @@
 $(call inherit-product, device/xiaomi/sm8450-common/common.mk)
 
 # Device identifiers
-PRODUCT_NAME := mondrian
+PRODUCT_NAME := aospa_mondrian
 PRODUCT_DEVICE := mondrian
 PRODUCT_BRAND := POCO
 PRODUCT_MODEL := POCO F5 Pro
 PRODUCT_MANUFACTURER := Xiaomi
 
-# Rootdir / Init files
+# Boot animation resolution tailored for WQHD+ display
+TARGET_BOOT_ANIMATION_RES := 1440
+
+# System Properties override
+TARGET_SYSTEM_PROP += $(LOCAL_PATH)/system.prop
+
+# Rootdir / Filesystem init packages
 PRODUCT_PACKAGES += \
     fstab.qcom \
     init.mondrian.rc
