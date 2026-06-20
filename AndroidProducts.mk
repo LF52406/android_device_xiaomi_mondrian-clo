6031d10de675b636f79558b0c0c1aa9e9ca02b17
LF5 <lf524prod24061993@gmail.com>
2026-06-21 00:52:20 +0300

    Add product definitions for mondrian

---
+++ AndroidProducts.mk
@@ -0,0 +1,8 @@
+# Product definitions for mondrian
+PRODUCT_MAKEFILES := \
+    $(LOCAL_DIR)/mondrian.mk
+
+COMMON_LUNCH_CHOICES := \
+    mondrian-user \
+    mondrian-userdebug \
+    mondrian-eng
